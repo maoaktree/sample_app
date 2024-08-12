bef5e4a05f499821c51d9c100259c3758aa1678b
maoaktree <mari.oaktree@gmail.com>
2024-08-12 18:14:35 -0300

    Update home_page.dart appBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -12,26 +11,103 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.transparent,
-      elevation: 0,
-      leading: Padding(
-        padding: const EdgeInsets.only(left: 24.0),
-        child: Icon(
-          Icons.menu,
-          color: Colors.grey[800],
-          size: 36,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            children: [
+              // home row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Column(
+                    children: [
+                      Text(
+                        "Delivery",
+                        style: TextStyle(
+                          color: Colors.blueGrey,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        'Araraquara, Sp',
+                        style: TextStyle(color: Colors.blueGrey),
+                      ),
+                    ],
+                  ),
+
+                  // account button
+                  IconButton(
+                    icon: Image.asset(
+                      'lib/images/user.png',
+                      height: 42,
+                      width: 42,
+                    ),
+                    onPressed: () {
+                      // account informations
+                    },
+                  ),
+                ],
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              // buttons
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: const Row(
+                      children: [
+                        // ORDER AGAIN
+                        Column(
+                          children: [
+                            Text(
+                              'ORDER AGAIN',
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+
+                  // LOCAL SHOP
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: const Row(
+                      children: [
+                        Column(
+                          children: [Text('LOCAL SHOP')],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 24.0),
-            child: Icon(
-              Icons.person, 
-              color: Colors.grey[800], 
-              size: 36,),
-            ),
-        ],
-        ),
+      ),
     );
   }
 }
