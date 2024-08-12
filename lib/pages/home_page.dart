9b07b88482127932cb5297768648e533141e92fc
maoaktree <mari.oaktree@gmail.com>
2024-08-12 15:11:34 -0300

    Create AppBar sketch in home_page.dart

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.transparent,
+      elevation: 0,
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 24.0),
+        child: Icon(
+          Icons.menu,
+          color: Colors.grey[800],
+          size: 36,
+        ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(
+              Icons.person, 
+              color: Colors.grey[800], 
+              size: 36,),
+            ),
+        ],
+        ),
+    );
+  }
+}
